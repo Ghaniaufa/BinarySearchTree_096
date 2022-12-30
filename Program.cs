ed50956d2bcdec24e0116741a2a1d2c80ab178a5
Ghaniaufa <maula.ghani.ft21@mail.umy.ac.id>
2022-12-30 17:24:51 +0700

    Create Public void insert for insert a node and check the node

--- Program.cs
+++ Program.cs
@@ -33,6 +33,24 @@ namespace Binary_Search_Tree
         {
             ROOT = null; /* Initializing ROOT to null */
         }
-        
+        public void insert(string element) /* Insert a node in th ebinary search tree */
+        {
+            Node tmp, parent = null, currentNode = null;
+            Search(element, ref parent, ref currentNode);
+            if (currentNode != null) /* Check if the node to be inserted already inserted or not */
+            {
+                Console.WriteLine("Duplicate words not allowed");
+                return;
+            }
+            else /* If the spesiied node is not present */
+            {
+                tmp new Node(element, null, null);/* creates a Node*/
+                if (parent == null)/* if the trees is epmty*/
+                {
+                    ROOT = tmp;
+                }
+                else if (string.Compare(element, parent.info) <0)
+            }   
+        }
     }
 }
